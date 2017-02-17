7d6d373cbced6a88a9a9d8ba64a96db1226b3c46
own3mall <own3mall@gmail.com>
2017-02-17 04:24:55 -0700

    Transifex Language Updates - Generated by own3mall's auto script.

--- lang/German/modules/support.php
+++ lang/German/modules/support.php
@@ -22,8 +22,8 @@
  *
  */
 
-define('support_email_content', "You have received a support request from %s (%s).<br><br>Server: %s.<br><br>%s");
-define('message_has_been_sent', "Vielen Dank für Ihre Kontaktaufnahme! Ihre Nachricht wurde empfangen und wir werden darauf Antworten per e-Mail.");
+define('support_email_content', "Sie haben eine Supportanfrage von %s (%s) erhalten.<br><br>Server: %s.<br><br>%s");
+define('message_has_been_sent', "Vielen Dank für Ihre Kontaktaufnahme! Ihre Nachricht wurde empfangen und wir werden per E-Mail darauf Antworten.");
 define('please_describe_your_issue_below', "Bitte benutzen Sie das untenstehende Formular, um Unterstützung für Ihren Gameserver anzufordern.<br>Wir werden so schnell wie möglich antworten.");
 define('select_server', "Wählen Sie einen server");
 define('email_address', "E-Mail-Adresse");
@@ -31,8 +31,8 @@ define('subject', "Betreff");
 define('message', "Nachricht");
 define('email_must_be_filled_out', "Die e-Mail-Adresse muss ausgefüllt werden.");
 define('subject_must_be_filled_out', "Der Betreff muss ausgefüllt werden.");
-define('message_must_be_filled_out', "The message must be filled out.");
+define('message_must_be_filled_out', "Die Nachricht muss ausgefüllt werden.");
 define('send', "Senden");
 define('error', "Fehler");
-define('info', "Info");
+define('info', "Information");
 ?>
