79ba117cb357ba10863ce3105f87be901b5195a0
own3mall <own3mall@gmail.com>
2017-09-14 00:49:41 -0600

    Transifex Language Updates - Generated by own3mall's auto script.

---
+++ lang/Portuguese(Brazil)/modules/support.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ *
+ * OGP - Open Game Panel
+ * Copyright (C) 2008 - 2017 The OGP Development Team
+ *
+ * http://www.opengamepanel.org/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+define('support_email_content', "Você recebeu uma solicitação de suporte de %s (%s).<br><br>Server: %s.<br><br>%s");
+define('message_has_been_sent', "Obrigado por nos contatar! Sua mensagem foi recebida e nós responderemos por email.");
+define('please_describe_your_issue_below', "Use o formulário abaixo para solicitar suporte para o seu servidor de jogos..<br>Nós responderemos o mais rapidamente possível.");
+define('select_server', "Escolha um servidor");
+define('email_address', "Endereço de e-mail");
+define('subject', "Assunto");
+define('message', "Mensagem");
+define('email_must_be_filled_out', "O endereço de e-mail deve ser preenchido.");
+define('subject_must_be_filled_out', "O assunto deve estar preenchido.");
+define('message_must_be_filled_out', "A mensagem deve ser preenchida.");
+define('send', "Enviar");
+define('error', "Erro");
+define('info', "Info");
+?>
