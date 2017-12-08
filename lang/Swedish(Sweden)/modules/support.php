eca1ec0228ba9c1354db0aa361795227514c9acf
own3mall <own3mall@gmail.com>
2017-12-07 17:08:30 -0700

    Transifex Language Updates - Generated by own3mall's auto script.

--- lang/Swedish(Sweden)/modules/support.php
+++ lang/Swedish(Sweden)/modules/support.php
@@ -22,17 +22,17 @@
  *
  */
 
-define('support_email_content', "You have received a support request from %s (%s).<br><br>Server: %s.<br><br>%s");
-define('message_has_been_sent', "Thank you for contacting us! Your message has been received and we will respond to it by email.");
-define('please_describe_your_issue_below', "Please use the form below to request support for your game server.<br>We will respond as soon as possible.");
-define('select_server', "Choose a server");
-define('email_address', "Email address");
-define('subject', "Subject");
-define('message', "Message");
-define('email_must_be_filled_out', "The email address must be filled out.");
-define('subject_must_be_filled_out', "The subject must be filled out.");
-define('message_must_be_filled_out', "The message must be filled out.");
-define('send', "Send");
-define('error', "Error");
-define('info', "Info");
+define('OGP_LANG_support_email_content', "You have received a support request from %s (%s).<br><br>Server: %s.<br><br>%s");
+define('OGP_LANG_message_has_been_sent', "Thank you for contacting us! Your message has been received and we will respond to it by email.");
+define('OGP_LANG_please_describe_your_issue_below', "Please use the form below to request support for your game server.<br>We will respond as soon as possible.");
+define('OGP_LANG_select_server', "Choose a server");
+define('OGP_LANG_email_address', "Email address");
+define('OGP_LANG_subject', "Subject");
+define('OGP_LANG_message', "Message");
+define('OGP_LANG_email_must_be_filled_out', "The email address must be filled out.");
+define('OGP_LANG_subject_must_be_filled_out', "The subject must be filled out.");
+define('OGP_LANG_message_must_be_filled_out', "The message must be filled out.");
+define('OGP_LANG_send', "Send");
+define('OGP_LANG_error', "Error");
+define('OGP_LANG_info', "Info");
 ?>
